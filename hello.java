64d395613bc51c39e899358c5efb5878241ee35e
Lóczy Levente Attila <71691777+loczylevi@users.noreply.github.com>
2026-02-10 17:26:55 +0100

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,17 @@
+package hu.sajat.helloworld;
+
+
+
+public class hello{
+	public static void main(String[] args) {
+		System.out.println("Chrommy Chromy");
+		System.out.println("yes papa?");
+		System.out.print("Eatings RAM?\n");        // \n itt is mukodik
+		System.out.println("no papa");
+		System.out.println("Telling Lies?");
+		System.out.println("no papa");
+		System.out.println("Open your mouth!");
+		
+		
+	}
+}
---
+++ valtozok.java
@@ -0,0 +1,40 @@
+package hu.sajat.helloworld;
+
+import java.util.Scanner;// input() fügvényt be kell importálni wtf man ház ez nem python skullemoji 
+
+public class valtozok {
+	public static void main(String[] args) {
+		System.out.print("Hell yeah\n xd \t dsad \n");    // legalább a \n itt is létezik na a tab is
+		
+		//______________________________//
+		String nev = "Gipsz Jakab";           // elöre meg kell adni a tipusáát a string az valamiért nagy betű xd
+		int magassag = 170; // cm
+		double kilo = 90.5;
+		boolean szobatiszta = false;
+		int f = 0;
+		f ++;
+		//String beker = "";
+		
+		//______________________________//
+		Scanner input = new Scanner(System.in);
+		
+		
+		System.out.print(nev + " Adatai:\n");
+		System.out.println("Magasság: " + magassag);
+		System.out.println("súly: " + kilo);
+		System.out.println("Szobatiszta: " + szobatiszta);
+		System.out.print("Add meg a neved: ");
+        String beker = input.nextLine(); 
+        System.out.print("Hány éves vagy? ");
+        int kora = input.nextInt();
+        System.out.print("Hány kiló vagy? ");
+        double sulya = input.nextDouble();
+        
+        
+        System.out.println("Az ön neve: " + beker);
+        System.out.println(kora + " éves");
+        System.out.println(sulya + " kg");
+        System.out.println(f);
+	}
+
+}
