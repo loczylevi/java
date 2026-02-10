dd374b9bbd6ca95e23a72ca39975da9121efd7f8
Lóczy Levente Attila <71691777+loczylevi@users.noreply.github.com>
2026-02-10 18:15:56 +0100

    Update szia_lajos.java

--- Ooprog_1_bev_tasks/szia_lajos.java
+++ Ooprog_1_bev_tasks/szia_lajos.java
@@ -1,5 +1,7 @@
 package gyaks2;
 
+// rjunk egy Hello World alkalmazást Eclipse alatt! Futtassuk le a programot!
+
 public class hello {
 	public static void main(String[] args) {
 		System.out.println("Szia Lajos!");
